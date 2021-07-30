2f09e303f1520c45dd9cef43fea58675383e9bee
Jonathan <jonathanfernandezfm@gmail.com>
2021-07-30 17:36:04 +0200

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<div></div>
+			</Head>
+
+			<main></main>
+
+			<footer></footer>
+		</div>
+	);
+}
