ca8ecdd21e2a19154e4a2127b548d680259419cd
Jonathan FM <jonathanfernandezfm@gmail.com>
2021-10-16 13:57:57 +0200

    Retoques mascara

--- components/MaskOverlay.tsx
+++ components/MaskOverlay.tsx
@@ -5,9 +5,9 @@ const MaskOverlay = () => {
 		<div>
 			<div
 				style={{ backgroundImage: 'url("./noise.gif")' }}
-				className="absolute top-0 left-0 z-20 w-full h-screen bg-center opacity-14"
+				className="absolute top-0 left-0 z-20 w-full h-screen bg-center opacity-8"
 			></div>
-			<div className="absolute top-0 left-0 z-20 w-full h-screen bg-black opacity-40"></div>
+			<div className="absolute top-0 left-0 z-20 w-full h-screen bg-black opacity-25"></div>
 		</div>
 	);
 };
